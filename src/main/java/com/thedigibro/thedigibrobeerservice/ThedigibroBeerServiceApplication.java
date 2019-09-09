052f6183beef80cdb94fb4506e2dadf95f3e2e61
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-09-09 11:18:09 +0200

    Initial commit

---
+++ src/main/java/com/thedigibro/thedigibrobeerservice/ThedigibroBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thedigibro.thedigibrobeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThedigibroBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThedigibroBeerServiceApplication.class, args);
+    }
+
+}
